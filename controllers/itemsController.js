e044473833c24d0b6869ffc0cd7ce502f9ca61ce
thryec <thryec@gmail.com>
2021-12-14 11:48:56 +0800

    added env example file, added test data in itemsController for checking frontend calls

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -3,7 +3,25 @@ const app = express()
 const Item = require('../models/itemsModel')
 
 app.get('/', (req, res) => {
-  res.send('items controller')
+  let seedData = [
+    {
+      username: 'merlin',
+      password: 'countryroads',
+      messages: ['teamwork is paramount', 'take me home, country road'],
+    },
+    { username: 'lancelot', password: 'roxy' },
+    {
+      username: 'eggsy',
+      password: 'galahadjr',
+      messages: ["who's in the mood for saving the world?"],
+    },
+    {
+      username: 'galahad',
+      password: 'harry',
+      messages: ['manners maketh man', "we're the new knights"],
+    },
+  ]
+  res.send(seedData)
 })
 
 module.exports = app
