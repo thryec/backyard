ceb252655c3e10529b72ee9d39e8cf37008769c1
thryec <thryec@gmail.com>
2021-12-14 19:52:25 +0800

    created seed data for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const User = require('../models/usersModel')
 const methodOverride = require('method-override')
+const usersSeed = require('../models/usersSeed')
 
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('users controller')
+  res.send(usersSeed)
 })
 
 module.exports = app
---
+++ models/usersSeed.js
@@ -0,0 +1,22 @@
+const usersSeed = [
+  {
+    username: 'merlinisawizard',
+    password: 'abc123',
+    email: 'merlin@camelot.com',
+    walletAddress: '0xA31110FcB12f67Aba26fCa7521B84170B09A7de0',
+  },
+  {
+    username: 'longlivethequeen',
+    password: 'abc123',
+    email: 'morganlefay@camelot.com',
+    walletAddress: '0xA32094FEB9c23A7c3A2bD01a507c9d02CB76F7b8',
+  },
+  {
+    username: 'arthurthedumbass',
+    password: 'abc123',
+    email: 'arthur@camelot.com',
+    walletAddress: '0xA33e1C8021F2f1b971aaA64838D1440c2826824e',
+  },
+]
+
+module.exports = usersSeed
