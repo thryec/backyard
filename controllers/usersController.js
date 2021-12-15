e5708e9487ff639714accc4f2d7a464e0a9fa59a
saetan <saetan@live.com>
2021-12-15 21:43:53 +0800

    checked CRUD is working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,20 +7,28 @@ const usersSeed = require('../models/usersSeed')
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send(usersSeed)
+// Seed data
+app.get('/seed', async (req, res) => {
+  for (let user of usersSeed) {
+    await User.create(user);
+  }
+})
+
+app.get('/', async (req, res) => {
+  const users = await User.find();
+  res.send(users);
 })
 
 app.post('/', async (req, res) => {
   console.log(req.body)
-  const user = await User.create(req.body)
-  res.send(user)
+  const user = await User.create(req.body);
+  res.send(user);
 })
 
 app.delete('/:id', async (req, res) => {
-  const user = await User.findOneAndDelete({ _id: req.params.id })
-  res.send(user)
-})
+  const user = await User.findOneAndDelete({ _id: req.params.id });
+  res.send(user);
+});
 
 app.put('/:id', async (req, res) => {
   const user = await User.updateOne({ _id: req.params.id }, req.body, {
