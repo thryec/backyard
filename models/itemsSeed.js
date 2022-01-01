714872b7450bb34bca76b019b223359e57c77618
thryec <thryec@gmail.com>
2022-01-01 09:50:08 +0800

    adjusted seed data to include seller

--- models/itemsSeed.js
+++ models/itemsSeed.js
@@ -5,6 +5,7 @@ const itemsSeed = [
     image: 'https://m.media-amazon.com/images/I/718dqDf4MmL._AC_SL1500_.jpg',
     price: 0.5,
     quantity: 1,
+    seller: '0xe82d5C6B394D9C4dE32F0913e6cE82Dd8dc39226',
   },
   {
     name: "Morgana's Wand",
@@ -12,6 +13,7 @@ const itemsSeed = [
     image: 'https://m.media-amazon.com/images/I/71huqcOKa+L._AC_SL1500_.jpg',
     price: 1,
     quantity: 1,
+    seller: '0xe82d5C6B394D9C4dE32F0913e6cE82Dd8dc39226',
   },
   {
     name: 'Excalibur',
@@ -19,6 +21,7 @@ const itemsSeed = [
     image: 'https://gbf.wiki/images/thumb/f/fa/Excalibur.png/462px-Excalibur.png',
     price: 0.75,
     quantity: 1,
+    seller: '0x78bCA437E8D6c961a1F1F7D97c81781044195bcF',
   },
 ]
 module.exports = itemsSeed
